e5118f1877ce1ae3af323125a4154c2e9e7be135
Nick <nickdbailey20@gmail.com>
2022-03-13 20:58:06 -0500

    splitting into separate APIs, adding src IP restriction for adds and throttling for code validation

---
+++ add_code_lambda.py
@@ -0,0 +1,159 @@
+import boto3
+from botocore.exceptions import ClientError
+import os, json, logging
+from secrets import randbelow
+
+
+UNKNOWN_ERROR = {
+    "statusCode": 503,
+    "body": "Internal failure"
+}
+
+UNKNOWN_ROUTE = {
+    "statusCode": 404,
+    "body": "Not found"
+}
+
+SUCCESS_CODE_ADD = {
+    "statusCode": 200,
+    "body": "New code: "
+}
+
+CODE_LENGTH = 20
+
+
+## Wrapper around AWS library methods to read/write to/from file in storage using provided API client
+## If no codebank exists (first execution), create new one
+## Returns codebank if action == read, otherwise False
+## If action == write, returns True or False depending on success
+def read_write_codebank(action, s3_object, codebank_data=None):
+
+    if action == 'read':
+        ## Get object from storage
+        try:
+            res = s3_object.get()
+            file_content = res['Body'].read().decode('utf-8')
+            logging.info('Successfully retrieved codebank from storage')
+
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                logging.info('First-time setup detected, creating empty codebank')
+                file_content = '{"unused_codes": [], "expired_codes": []}'
+            else:
+                logging.error(f'Could not retrieve codebank, exiting: {e}')
+                return False
+        
+        ## Load object content
+        try:
+            codebank = json.loads(file_content)
+            logging.info('Successfully parsed codebank')
+            return codebank
+
+        except:
+            logging.error('Could not read codebank, exiting')
+            return False
+
+
+    elif action == 'write':
+        ## Write object back to storage
+        try:
+            s3_object.put(
+                Body=(bytes(json.dumps(codebank_data).encode('utf-8')))
+            )
+            logging.info('Successfully wrote updated codebank to storage')
+            return True
+
+        except ClientError as e:
+            logging.error(f'Could not write codebank to storage, exiting: {e}')
+            return False
+
+
+
+def generate_random_code():
+    charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    charset += 'abcdefghijklmnopqrstuvwxyz'
+    charset += '0123456789'
+    charset += '_-'
+    charset = list(charset)
+    
+    num_chars = len(charset)
+    code = ''
+
+    for _ in range(CODE_LENGTH):
+        idx = randbelow(num_chars)
+        code += charset[idx]
+
+    return code
+
+
+
+## Adds new code to codebank and saves to storage
+## Returns HTTP response based on success
+def add_new_code(codebank_object):
+
+    codebank = read_write_codebank('read', codebank_object)
+    if not codebank:
+        return UNKNOWN_ERROR
+
+    ## Generate random code value that doesn't already exist in codebank
+    existing_code = True
+    while existing_code:
+        new_code = generate_random_code()
+
+        if new_code in codebank['expired_codes'] or new_code in codebank['unused_codes']:
+            logging.info(f'Generated previously seen code {new_code}, trying again...')
+
+        else:
+            logging.info(f'Generated new valid code {new_code}')
+            existing_code = False
+        
+
+    ## Add new code to codebank and save
+    codebank['unused_codes'].append(new_code)
+    logging.info(f'Successfully added new code {new_code} to codebank')
+
+    successful_write = read_write_codebank('write', codebank_object, codebank_data=codebank)
+    if not successful_write:
+        return UNKNOWN_ERROR
+
+
+    ## Provide new code back in response
+    SUCCESS_CODE_ADD['body'] += new_code
+    return SUCCESS_CODE_ADD
+
+
+
+
+## Example API usage: <apidomain>.amazonaws.com/generate_code
+##
+## Codebank format: {'unused_codes': [], 'expired_codes': []}
+def lambda_handler(event, _):
+    logging.getLogger().setLevel(logging.INFO)
+
+    logging.info(f'Invoked AddCode API with event: {event}')
+
+    ## Get environment variables needed for script
+    try:
+        bucket = os.environ['download_bucket']
+        codebank_name = 'codebank.json'
+    except:
+        logging.error('Could not retrieve environment variables needed for script, exiting')
+        return UNKNOWN_ERROR
+    
+
+    ## Create AWS storage API client with Lambda user creds to interact with codebank/game files
+    try:
+        s3 = boto3.resource('s3')
+        codebank_object = s3.Object(bucket, codebank_name)
+
+    except ClientError as e:
+        logging.error(f'Could not create S3 bucket resource using IAM role, exiting: {e}')
+        return UNKNOWN_ERROR
+
+
+    ## If expected URL path, generate, save, return new valid code 
+    path = event['path'][1:] ## strip off leading /
+    if path == 'generate_code':
+        return add_new_code(codebank_object)
+    else:
+        return UNKNOWN_ROUTE
---
+++ check_code_lambda.py
@@ -0,0 +1,145 @@
+import boto3
+from botocore.exceptions import ClientError
+import os, json, logging
+
+
+
+INVALID_DOWNLOAD_CODE = {
+    "statusCode": 403,
+    "body": "Invalid download code."
+}
+
+EXPIRED_DOWNLOAD_CODE = {
+    "statusCode": 405,
+    "body": "Expired download code."
+}
+
+VALID_DOWNLOAD_CODE = {
+    "statusCode": 201,
+    "body": "Valid download code."
+}
+
+UNKNOWN_ERROR = {
+    "statusCode": 503,
+    "body": "Internal failure."
+}
+
+
+CODEBANK_FILE_NAME = 'codebank.json'
+
+
+## Wrapper around AWS library methods to read/write to/from file in storage using provided API client
+## If no codebank exists (first execution), create new one
+## Returns codebank if action == read, otherwise False
+## If action == write, returns True or False depending on success
+def read_write_codebank(action, s3_object, codebank_data=None):
+
+    if action == 'read':
+        ## Get object from storage
+        try:
+            res = s3_object.get()
+            file_content = res['Body'].read().decode('utf-8')
+            logging.info('Successfully retrieved codebank from storage')
+
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                logging.info('First-time setup detected, creating empty codebank')
+                file_content = '{"unused_codes": [], "expired_codes": []}'
+            else:
+                logging.error(f'Could not retrieve codebank, exiting: {e}')
+                return False
+        
+        ## Load object content
+        try:
+            codebank = json.loads(file_content)
+            logging.info('Successfully parsed codebank')
+            return codebank
+
+        except:
+            logging.error('Could not read codebank, exiting')
+            return False
+
+
+    elif action == 'write':
+        ## Write object back to storage
+        try:
+            s3_object.put(
+                Body=(bytes(json.dumps(codebank_data).encode('utf-8')))
+            )
+            logging.info('Successfully wrote updated codebank to storage')
+            return True
+
+        except ClientError as e:
+            logging.error(f'Could not write codebank to storage, exiting: {e}')
+            return False
+
+
+
+## Changes codebank code from unused to expired, 
+## Returns False if code already expired, otherwise updated codebank
+def expire_used_code(codebank, code):
+    if code in codebank['expired_codes']:
+        logging.info(f'Code {code} is already expired, aborting')
+        return False
+
+    codebank['expired_codes'].append(code)
+    codebank['unused_codes'].remove(code)
+    logging.info(f'Changing code {code} to expired')
+    return codebank
+
+
+
+## Checks provided code against unused_codes in codebank and sets value to expired if match
+## Returns appropriate HTTP response for given code
+def check_code(input_code, codebank_object):
+    codebank = read_write_codebank('read', codebank_object)
+
+    if input_code in codebank['expired_codes']:
+        return EXPIRED_DOWNLOAD_CODE
+
+    if input_code in codebank['unused_codes']:
+        updated_codebank = expire_used_code(codebank, input_code)
+
+        if not updated_codebank:
+            return UNKNOWN_ERROR
+
+        read_write_codebank('write', codebank_object, codebank_data=updated_codebank)
+        return VALID_DOWNLOAD_CODE
+
+    return INVALID_DOWNLOAD_CODE
+
+
+
+## Example API usage: <apidomain>.amazonaws.com/12345 to validate code '12345'
+##
+## Codebank format: {'unused_codes': [], 'expired_codes': []}
+def lambda_handler(event, _):
+    logging.getLogger().setLevel(logging.INFO)
+
+    logging.info(f'Invoked CheckCode API with event: {event}')
+
+
+    ## Get environment variables needed for script
+    try:
+        bucket = os.environ['download_bucket']
+        codebank_name = CODEBANK_FILE_NAME
+    except:
+        logging.error('Could not retrieve environment variables needed for script, exiting')
+        return UNKNOWN_ERROR
+    
+
+    ## Create AWS storage API client with Lambda user creds to interact with codebank/game files
+    try:
+        s3 = boto3.resource('s3')
+        codebank_object = s3.Object(bucket, codebank_name)
+
+    except ClientError as e:
+        logging.error(f'Could not create S3 bucket resource using IAM role, exiting: {e}')
+        return UNKNOWN_ERROR
+
+
+    input_code = event['path'][1:] ## strip off leading /
+    http_response = check_code(input_code, codebank_object)
+
+    logging.info(f'Sending HTTP response {http_response}, script complete.')
+    return http_response
