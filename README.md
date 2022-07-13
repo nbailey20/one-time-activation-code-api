b966e27651e9f4ee957474149f00057756470e24
Nick <nickdbailey20@gmail.com>
2022-07-12 19:08:11 -0500

    IAM user restriction instead of IP

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # AWS-Hosted One-Time Activation Code Generation & Validation APIs
 
-## Last Updated: 4/20/22
-## Version: 1.0.1
+## Latest Version: 1.0.2
+
+# Release Notes
+## v1.0.2
+* Updated: 7/12/22
+* Locking down API to IAM user instead of IP for actual security
+
+## v1.0.1
+* Updated: 4/20/22
+* Split into 2 APIs: one public, one locked down to specific IP address
+
+## v1.0.0
+* Initial working POC, single API with multiple routes
+
+
+# Usage Instructions
+* Create CloudFormation stack with uploaded create_api.yml template, enter desired parameters, acknowledge creation of IAM resources
+* Create access credentials for new user in IAM service and save locally with "aws configure" command in command prompt (requires AWS CLI)
+* Use access credentials to invoke AddCode API, 'python test_app.py' for example invocation
+* Invoke CheckCode API with code from AddCode response, should be Valid code.
+* To delete/recreate, first delete S3 bucket codebank.json, then delete stack
+* If recreating brand new, API invoke URL will change! Rotate credentials from IAM service if needed
---
+++ test_app.py
@@ -0,0 +1,22 @@
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+import boto3
+import requests
+
+## CHANGE ME TO CORRECT VALUES TO TEST ADDCODE API #########################################################
+hostname = ""                         ## Example: <API ID>.execute-api.us-east-1.amazonaws.com, NO /v1/!
+user_profile = "one-time-download"    ## profile name created during 'aws configure' command
+#############################################################################################################
+
+session = boto3.Session(profile_name=user_profile)
+credentials = session.get_credentials()
+
+print("Starting test script to generate new download code by invoking AddCode API...")
+auth = AWSRequestsAuth(aws_access_key=credentials.access_key,
+                       aws_secret_access_key=credentials.secret_key,
+                       aws_token = credentials.token,
+                       aws_host=hostname,
+                       aws_region='us-east-1',
+                       aws_service='execute-api')
+
+response = requests.get('https://'+hostname+'/v1/generate_code', auth=auth)
+print(response.content.decode('ascii'))
